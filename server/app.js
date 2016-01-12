65e237fbf90ed69cae5f334829d1edfbd43ca7b1
jmichelin <jmichelinphone@gmail.com>
2016-01-12 13:01:03 -0800

    [Add] Adds bookshelf to app

--- server/app.js
+++ server/app.js
@@ -1,10 +1,22 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+'use strict'
 
-var app = express();
-console.log(__dirname );
+//define module requirements
+let express = require('express');
+let bodyParser = require('body-parser');
+
+
+//create instance of express app as app
+let app = express();
+
+
+//create database connection
+
+
+//serve static assets
 app.use(express.static(__dirname + '/..' + '/client'))
 
+
+//start simple server listening
 app.listen(8000, function() {
   console.log("✔ Express server listening on port %d", 8000, app.get('env'));
 });

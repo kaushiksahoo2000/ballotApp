96e9c71bdadeffdcddef279669d3bf44f1870673
kaushiksahoo2000 <kaushiksahoo2000@gmail.com>
2016-01-15 12:27:40 -0800

    [progress] working on user voting ability

--- client/app/scripts/controllers/uservotingpage.js
+++ client/app/scripts/controllers/uservotingpage.js
@@ -8,18 +8,21 @@
  * Controller of the ballotTempApp
  */
 angular.module('angularBestPracticeApp')
-  .controller('UserVotingPageCtrl',function($scope, $http, $rootScope){
+  .controller('UserVotingPageCtrl',function($scope, $http, $rootScope, $timeout){
+    $timeout(function(){
     $http.get("/api/ballots/" + $scope.userGivenCode)
     .success(function(data){
       $scope.ballotData = data;
       $scope.userTopic = $scope.ballotData.data.ballot_name;
       console.log("this is $scope.ballotData", $scope.ballotData);
+      console.log("this is $rootScope.ballotId in uservotingpage", $rootScope.ballotId);
       $scope.choices[0].choice = $scope.ballotData.data.ballot_option_one;
       $scope.choices[1].choice = $scope.ballotData.data.ballot_option_two;
       $scope.choices[2].choice = $scope.ballotData.data.ballot_option_three;
       $scope.choices[3].choice = $scope.ballotData.data.ballot_option_four;
       $scope.choices[4].choice = $scope.ballotData.data.ballot_option_five;
     });
+    }, 1000);
 
     $scope.choices = [
       {
@@ -39,20 +42,31 @@ angular.module('angularBestPracticeApp')
       }
     ];
 
-    // $scope.buttonColor = false;
-    // $scope.changeButtonColor = function(){
-    //   $scope.buttonColor = !$scope.buttonColor;
-    // };
 
     $scope.selectedIndex = 0;
+    $scope.userVoterChoice = '';
 
     $scope.select = function(i){
       $scope.selectedIndex = i;
       console.log(this.choice.choice);
+      $scope.userVoterChoice = this.choice.choice;
+      console.log("this is the updating userVoterChoice", $scope.userVoterChoice);
     };
 
     $scope.endVoting = function(){
       console.log("inside endVoting function");
+      $http({
+          method  : 'POST',
+          url     : '/api/voter/vote',
+          data    : {
+            "ballotId": $rootScope.ballotId,
+            "userVoteChoice": $scope.userVoterChoice
+          }
+        }).then(function(success){
+          console.log(success);
+        }, function(err){
+          console.log('THIS IS AN ERROR!');
+        });
     };
   });
 
